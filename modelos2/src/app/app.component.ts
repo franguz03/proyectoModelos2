ad754fdf4f9953db28e88b051d408883fa235400
franguz03 <francorjuela04@gmail.com>
2023-05-07 22:53:00 -0500

    funcion agregar espacios

--- modelos2/src/app/app.component.ts
+++ modelos2/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modelos2';
+
+  crearContenedorEnAside() {
+    console.log("helloo");
+
+    // Obtener nombre del proyecto
+    const nombreProyecto = prompt("¿Cómo se llama tu proyecto?");
+
+    // Obtener referencia al elemento <aside>
+    var asideElement = document.querySelector('aside');
+
+    if (asideElement !== null && nombreProyecto!=null) {
+      // Crear un nuevo contenedor
+      var nuevoContenedor = document.createElement('div');
+
+      // Agregar clases o atributos al nuevo contenedor si es necesario
+      //nuevoContenedor.className = 'mi-contenedor';
+      nuevoContenedor.style.backgroundColor = '#f2f2f2';
+      nuevoContenedor.style.padding = '10px';
+      nuevoContenedor.style.borderTop = '1px solid #d0d0d0';
+      nuevoContenedor.style.borderBottom = '1px solid #d0d0d0';
+      nuevoContenedor.style.height='1.5rem'
+      nuevoContenedor.textContent = nombreProyecto;
+
+      // Agregar el nuevo contenedor al aside
+      asideElement.appendChild(nuevoContenedor);
+    }
+  }
 }
